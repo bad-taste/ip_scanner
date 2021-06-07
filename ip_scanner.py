60ab0ae1d1f378ce82c3d132b994ef244b66415b
bad-taste <70190280+bad-taste@users.noreply.github.com>
2021-06-07 17:18:06 +0300

    Update ip_scanner.py

--- ip_scanner.py
+++ ip_scanner.py
@@ -1,24 +1,40 @@
 import socket
 import time
 import threading
-
+from time import sleep
+#from mysql_connector import *
+from pprint import pprint
+from pprint import pformat
 from queue import Queue
+import sys
+from threading import Thread, Lock
+
+
+openPorts = []
+hostnameOpenPorts = {} #storing result for one hostname and list of open ports
+ipHostnameOpenPorts = {} #storing result for one ip, its hostname and opened ports
+finalResults = {} #storing all results
+targetHosts = {'ec2-18-185-241-122.eu-central-1.compute.amazonaws.com': '35.158.160.236'}
+queue = Queue()
 
 socket.setdefaulttimeout(0.25)
 print_lock = threading.Lock()
 
-#target = input('Enter the host to be scanned: ')
-targetIp = ('ip') #enter the ips to be scanned
-print('Starting scan on host: ', targetIp)
-
+print("The scanner is started. Please wait for results")
 
 def portscan(port):
-    for ip in targetIp:
+    for hostname, ip in targetHosts.items():
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
+
             con = s.connect((ip, port))
             with print_lock:
-                print(port, 'is open on', ip )
+
+                openPorts.append(port)
+                hostnameOpenPorts = {"hostname": hostname,
+                                "openports": list(set(openPorts))}
+                result = {ip: hostnameOpenPorts}
+                finalResults.update(result)
             con.close()
         except:
             pass
@@ -26,21 +42,24 @@ def portscan(port):
 
 def threader():
     while True:
-        worker = q.get()
+        worker = queue.get()
         portscan(worker)
-        q.task_done()
+        queue.task_done()
 
 
-q = Queue()
 startTime = time.time()
-
 for x in range(100):
     t = threading.Thread(target=threader)
     t.daemon = True
     t.start()
 
-for worker in range(1, 65000):
-    q.put(worker)
+for worker in range(1, 10000):
+    queue.put(worker)
 
-q.join()
+queue.join()
+
 print('Time taken:', time.time() - startTime)
+
+print("----------------------------------------------")
+pprint(finalResults)
+print(len(finalResults))
